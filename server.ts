3d751cbd8a7c8bb40ed767e38585bc9cf69edced
Valimp <valentinchauvet.contact@gmail.com>
2024-02-28 21:19:09 +0100

    feat: mongodb connection w. mongoose

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import swaggerJSDOC from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { PORT } from './const/config';
+import { PORT, MONGODB_URI } from './const/config';
 import { swaggerOptions } from './swaggerOptions';
 // Routes imports
 import authRouter from './router/auth';
@@ -14,6 +15,16 @@ app.use('/auth', authRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+// Connect to MongoDB and log message if successful
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+// App listening
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
